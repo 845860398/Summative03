e9d882a11558709b5900da21b6b6222c2dc0fe3e
Jordan Paese <jxvrdvn@7ucy.local>
2018-03-19 13:02:31 +1300

    jordan commit

--- js/jesse.js
+++ js/jesse.js
@@ -47,35 +47,35 @@ function initMap(){
         anchorPoint: new google.maps.Point(0, -29)
     });// this is ok
 
-    var map, infoWindow;
-    infoWindow = new google.maps.InfoWindow;
+    // var map, infoWindow;
+    // infoWindow = new google.maps.InfoWindow;
 
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+    //     if (navigator.geolocation) {
+    //       navigator.geolocation.getCurrentPosition(function(position) {
+    //         var pos = {
+    //           lat: position.coords.latitude,
+    //           lng: position.coords.longitude
+    //         };
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
+    //         infoWindow.setPosition(pos);
+    //         infoWindow.setContent('Location found.');
+    //         infoWindow.open(map);
+    //         map.setCenter(pos);
+    //       }, function() {
+    //         handleLocationError(true, infoWindow, map.getCenter());
+    //       });
+    //     } else {
+    //       // Browser doesn't support Geolocation
+    //       handleLocationError(false, infoWindow, map.getCenter());
+    //     }
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-    }
+    //   function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    //     infoWindow.setPosition(pos);
+    //     infoWindow.setContent(browserHasGeolocation ?
+    //                           'Error: The Geolocation service failed.' :
+    //                           'Error: Your browser doesn\'t support geolocation.');
+    //     infoWindow.open(map);
+    // }
 
     // autoComplete event
     autocomplete.addListener('place_changed', function() {
